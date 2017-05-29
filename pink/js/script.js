f5c539864d601c94da4a91c6b3bacabee7067b86
djatle <djatle@mail.ru>
2017-05-29 14:48:33 +0300

    0.0000001 beta

--- pink/js/script.js
+++ pink/js/script.js
@@ -29,7 +29,7 @@ $(window).resize(function(){
 			
 		var w_width = $(window).width();
 		
-		if ( w_width > 700){
+		if ( w_width > 980){
 				
 			hideFunc(mobile_menu);
 			hideFunc(show_btn);
@@ -50,34 +50,98 @@ function blockFunc(a){
 function hideFunc(a){
 	a.css("display", "none");
 }
-/////////////////////////////////////////////////////////////////
+///////////////Последовательное изменение изображений в мобильной версии///////////
 	
 	var i_case = $(".phone-frame");
 	var re = window.devicePixelRatio;
 	
-	
-	
-	
-	var img_array_1x = [
+	//Содержимое массива img_array от разрешающей способности экрана. Значение переменной re становится
+    //частью пути к файлу. конструкция страдает некоторой громоздкостью, но избавляет от необходимости создавть 3 отдельных массива	
+	var img_array = [
 							"url(img/iphone-screen-1-1@"+re+"x.png), url(img/iphone-case-1@"+re+"x.png)",
 							"url(img/iphone-screen-1-2@"+re+"x.png), url(img/iphone-case-1@"+re+"x.png)",
 							"url(img/iphone-screen-1-3@"+re+"x.png), url(img/iphone-case-1@"+re+"x.png)"
-							
-					   ];
-		
+					];
 	
 	var i = 0;	
 	var timerId = setInterval(function(){
 				    	
-			i_case.css("backgroundImage", img_array_1x[i]);
+			i_case.css("backgroundImage", img_array[i]);
 			i++;
-			if(i > img_array_1x.length){
+			
+			if(i > img_array.length){
 				i = 0;
 			}
-						
 	}
-		
 	, 2000);
 	
-	alert($(window).width());
+///////////////////////////////////////////////////////////////	
+	
+	
+///////////Заполнение ячеек таблицы тарифных планов////////////	
+	var yes = $(".yes-mark").html();
+	var no = $(".no-mark").html();
+	
+	var cells = $(".feauture-exist");
+	
+		for(i = 0; i < cells.length; i++)
+		{
+			var res = cells[i].innerHTML;
+
+			if(res == 'yes')
+			{
+				$(cells[i]).css("fill", "green").html(yes);
+			}
+			
+			if(res == 'no')
+			{
+				$(cells[i]).css("fill", "red").html(no);
+			}	
+					
+		}
+		
+/////////////-------Слайдер для десктопной версии-------////////////////////
+	var slideWidth=680;
+	var sliderTimer;
+	
+	$(function(){
+$('.slidewrapper').width($('.slidewrapper').children().length*slideWidth);
+    sliderTimer=setInterval(nextSlide,2000);
+    $('.viewport').hover(function(){
+        clearInterval(sliderTimer);
+    },function(){
+        sliderTimer=setInterval(nextSlide,2000);
+    });
+    $('.right-arrow').click(function(){
+		clearInterval(sliderTimer);
+        nextSlide();
+        sliderTimer=setInterval(nextSlide,2000);
+    });
+    $('.left-arrow').click(function(){
+        clearInterval(sliderTimer);
+        prevSlide();
+        sliderTimer=setInterval(nextSlide,2000);
+    });
+});
+
+	function nextSlide(){
+		var currentSlide=parseInt($('.slidewrapper').data('current'));
+		currentSlide++;
+		if(currentSlide>=$('.slidewrapper').children().length)
+		{
+			currentSlide=0;   
+		}
+		$('.slidewrapper').animate({left: -currentSlide*slideWidth},680).data('current',currentSlide);
+	}
+
+	function prevSlide(){
+		var currentSlide=parseInt($('.slidewrapper').data('current'));
+		currentSlide--;
+		if(currentSlide<0)
+		{
+			currentSlide=$('.slidewrapper').children().length-1;   
+		}
+		$('.slidewrapper').animate({left: -currentSlide*slideWidth},680).data('current',currentSlide);
+	}
+			
 });
